22ac256052a6e60fd1f3548b13d7c6a24cd8859a
Juan Daniel Carreño Rodriguez <126117081+JDaniec@users.noreply.github.com>
2023-03-04 09:51:05 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,17 @@
+print("----------------------------------------------------------------------------------------")
+print("--------------Determinar si los ultimo digitos de un numero son iguales-----------------")
+print("----------------------------------------------------------------------------------------")
+
+
+# input
+
+n = int(input("Digite un numero: "))
+
+#processing
+if -10 <= n <= 10:
+    rta = "El numero no tiene mas de dos digitos"
+
+else:
+    
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Determinar-los-ultimos-dos-digitos-son-iguales
 diseñar y construir un programa que lea un numero entero y que determine si sus dos últimos son iguales 
+![Diagrama de flujo](Diagrama.png "Diagrama de flujo")
